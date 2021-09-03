278845343f1ba62b8d20bb9eb7f786b90fa8e0d1
bees <88304894+beesnerd@users.noreply.github.com>
2021-09-03 16:43:35 -0700

    Create autoclick.py
    the whole thing

---
+++ autoclick.py
@@ -0,0 +1,23 @@
+import pyautogui
+from pyautogui import time
+
+
+answer = input("would you like to left click or right click? (Right/Left) (case sensitive)")
+
+if answer == 'Left':
+    time.sleep(7)
+    while True:
+        for i in range(100):
+            pyautogui.PAUSE = 0.01
+            pyautogui.leftClick()
+
+
+if answer == 'Right':
+    time.sleep(7)
+    while True:
+        for i in range(100):
+            pyautogui.PAUSE = 0.01
+            pyautogui.rightClick()
+
+
+
